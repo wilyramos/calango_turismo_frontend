fdb5b582bd21d9d4eed9b2b199691b3a2a4e6b86
wily ramos <wily.ramos1@unmsm.edu.pe>
2025-02-11 17:32:12 -0500

    fix pages vercel.json

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Header from "../components/Header";
 import Categorias from "../components/Categorias";
+import DestinosPrincipales from "../components/DestinosPrincipales";
 
 
 
@@ -7,11 +8,13 @@ export default function Home() {
     return (
         <>
           <Header />
+          <DestinosPrincipales />
             <div className="p-6 max-w-screen-lg mx-auto">
                 <h2 className="text-3xl font-bold text-center text-green-600">Encuentra tu siguiente parada</h2>
                 <p className="mt-4 text-gray-700 text-center">Descubre el turismo en calango recomendado para ti.</p>
                 <Categorias />
             </div>
+            
         </>
     );
 };
