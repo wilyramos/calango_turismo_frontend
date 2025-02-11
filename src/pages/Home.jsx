35cf73e4586620a88059199d0905a2bfae125137
wily ramos <wily.ramos1@unmsm.edu.pe>
2025-02-11 16:51:46 -0500

    fix deploy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ export default function Home() {
           <Header />
             <div className="p-6 max-w-screen-lg mx-auto">
                 <h2 className="text-3xl font-bold text-center text-green-600">Encuentra tu siguiente parada</h2>
-                <p className="mt-4 text-gray-700 text-center">Descubre un turismo personalizado recomendado para ti.</p>
+                <p className="mt-4 text-gray-700 text-center">Descubre el turismo en calango recomendado para ti.</p>
                 <Categorias />
             </div>
         </>
